925e86e7aec4a2001358916b640e770b64ea1914
Karthik Vibuthi <karthikvibuthi999@gmail.com>
2024-12-01 14:00:13 -0500

    web app

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import re
 
 app = Flask(__name__)
 
+def extract_email(text):
+    """Extracts email from resume text."""
+    email = re.findall(r"([^@|\s]+@[^@]+\.[^@|\s]+)", text)
+    return email[0].split()[0].strip(';') if email else None
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route("/", methods=["GET", "POST"])
+def index():
+    email = None
+    if request.method == "POST":
+        resume_text = request.form.get("resume_text")
+        if resume_text:
+            email = extract_email(resume_text)
+    return render_template("index.html", email=email)
 
-if __name__ == '__main__':
-    app.run(debug=True)  # run the app in debug mode
+if __name__ == "__main__":
+    app.run(debug=True)
