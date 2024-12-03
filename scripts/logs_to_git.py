229c6edf6ac3aa04c7b224f7beab35a27b96ea22
Karthik Vibuthi <karthikvibuthi999@gmail.com>
2024-12-03 11:53:46 -0500

    scraping automation

---
+++ scripts/logs_to_git.py
@@ -0,0 +1,20 @@
+import subprocess
+import os
+from datetime import datetime
+
+# GitHub repository path
+repo_dir = '/home/ubuntu/finalproject'
+
+# Log the status of the job
+log_file = '/home/ubuntu/finalproject/scripts/status_log.txt'
+log_message = f"{datetime.now()} - Job and Event scraping completed successfully.\n"
+
+# Append log message to the file
+with open(log_file, 'a') as f:
+    f.write(log_message)
+
+# Commit and push to GitHub
+os.chdir(repo_dir)
+subprocess.run(['git', 'add', 'scripts/status_log.txt'])
+subprocess.run(['git', 'commit', '-m', 'Updated scraping log'])
+subprocess.run(['git', 'push', 'origin', 'main'])
